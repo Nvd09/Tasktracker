1f9a256c6718e0a8328b5975fc15993c24a28386
Navid Khan <navidkhan@Navids-MacBook-Air.local>
2023-02-20 23:13:32 -0500

    Initial Commit

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent {
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    //Since getTasks() is an observable we need to subscribe
+    // to the output of this function
+    this.taskService
+      .getTasks()
+      .subscribe((taskOutput) => (this.tasks = taskOutput));
+  }
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+}
