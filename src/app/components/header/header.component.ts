dd5611130dec7d1291421e0b4b7e374be16515ab
Navid Khan <navidkhan@Navids-MacBook-Air.local>
2023-02-22 16:13:47 -0500

    Working Add Task Section with show and hide features

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   title = 'TaskTracker';
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
   toggleAddTask() {
-    console.log("button toggled")
-    }
+    this.uiService.toggleAddTask();
+  }
 }
